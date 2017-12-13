4f9ad49702261e578fd715cfaf8869cd76e40cb7
ukimiawz <deathmoon123>
2017-12-13 16:42:37 +0100

    Generated microservice version 1.0

---
+++ db/uat-image-2_create_tables.sql
@@ -0,0 +1,18 @@
+--
+-- Database Schema:  uatTest
+-- Automatically generated sql script for the service uat-image-2, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tblImage.
+--
+CREATE TABLE uatTest.tblImage (
+  musicId INT(11) ,
+  musicUrl VARCHAR(255) ,
+  musicName VARCHAR(255) ,
+CONSTRAINT musicId_PK PRIMARY KEY (musicId)
+ 
+);
+
+
+
