9397d4281357b1f5b00679d523b41b22149eab4c
ukimiawz <melisa.cecilia@yahoo.com>
2017-12-13 17:27:33 +0100

    Http Method[postImage] edited by Melisa Cecilia

--- src/main/i5/las2peer/services/uatTestImage/uatTestImage.java
+++ src/main/i5/las2peer/services/uatTestImage/uatTestImage.java
@@ -115,13 +115,26 @@ public class uatTestImage extends RESTService {
        JSONObject result = new JSONObject();
        return Response.status(HttpURLConnection.HTTP_INTERNAL_ERROR).entity("Cannot convert json to object").build();
    }
-    // responseImage
-    boolean responseImage_condition = true;
-    if(responseImage_condition) {
-      String resultImage = "Some String";
-      return Response.status(HttpURLConnection.HTTP_OK).entity(resultImage).build();
+    try { 
+        Connection conn = service.dbm.getConnection();
+        PreparedStatement query = conn.prepareStatement(
+          "INSERT INTO uatTest.tblImage(imageName, imageUrl) VALUES(?,?) ");
+        query.setString(1, payloadpayloadPostImageObject.getimageName());
+        query.setString(2, payloadpayloadPostImageObject.getimageUrl());
+        query.executeUpdate();
+
+        // get id of the new added image
+        ResultSet generatedKeys = query.getGeneratedKeys();
+        if (generatedKeys.next()) {
+          return Response.status(HttpURLConnection.HTTP_OK).entity(generatedKeys.getLong(1)).build();
+        } else {
+          return Response.status(HttpURLConnection.HTTP_OK).entity(0).build();
+        }
+    } catch(Exception e) {
+      e.printStackTrace();
+      return Response.status(HttpURLConnection.HTTP_INTERNAL_ERROR).entity(0).build();
     }
-    return null;
+
   }
 
   /**
